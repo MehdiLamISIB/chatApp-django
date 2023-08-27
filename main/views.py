046f2c4839c738111d981be35aab7729365ea7ba
MehdiLamISIB <52042@etu.he2b.be>
2023-08-27 11:54:14 +0200

    login

--- main/views.py
+++ main/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+### On importe built-in lib de django
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
 
-def chatPage(request,*args, **kwargs):
-    if not request.user.is_authenticated:
-        return redirect("login-user")
-    context={}
-    return render(request,"main/chatPage.html",context)
+
+
+def connect_user(request,user):
+    pass
+
+def home(request,user):
+    return render(request,template_name="main.html",context={'user_name':user.name})
+
+
+
+
+
+def login_view(request):
+    username=request.POST["username"]
+    password=request.POST["password"]
+    user=authenticate(request,username=username,password=password)
+    if user is not None:
+        ### Utilisateur existe
+        pass
+    else:
+        ### On le redirige vers la page d'inscription
+        pass
+def logout_view(request):
+    logout(request)
