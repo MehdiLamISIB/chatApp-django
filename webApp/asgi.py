56b18fbaf8a3ab40505109621f6956798f6e10cf
MehdiLamISIB <52042@etu.he2b.be>
2023-08-29 13:16:01 +0200

    finalisation (doit nettoyer fichier)

--- webApp/asgi.py
+++ webApp/asgi.py
@@ -8,30 +8,30 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 ### UTILISATION SANS CHANNELS
-#import os
-#from django.core.asgi import get_asgi_application
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webApp.settings')
-#application = get_asgi_application()
-
-##UTILISATION AVEC CHANNELS
 import os
 from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webApp.settings')
+application = get_asgi_application()
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter , URLRouter
-from main import routing
-
-application = ProtocolTypeRouter(
-	{
-		"http" : get_asgi_application() ,
-		"websocket" : AuthMiddlewareStack(
-			URLRouter(
-				routing.websocket_urlpatterns
-			)
-		)
-	}
-)
+##UTILISATION AVEC CHANNELS
+#import os
+#from django.core.asgi import get_asgi_application
+#
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webApp.settings')
+#
+#from channels.auth import AuthMiddlewareStack
+#from channels.routing import ProtocolTypeRouter , URLRouter
+#from main import routing
+#
+#application = ProtocolTypeRouter(
+#	{
+#		"http" : get_asgi_application() ,
+#		"websocket" : AuthMiddlewareStack(
+#			URLRouter(
+#				routing.websocket_urlpatterns
+#			)
+#		)
+#	}
+#)
 
 
